e0d401a01d6b2bb0aac180aed862b21c3343b992
--global <tokikawa@alte.co.jp>
2022-02-09 18:06:37 +0900

    アカウントを切り替えるコマンドを間違えていたので修正

--- GithubChangeAccount.bat
+++ GithubChangeAccount.bat
@@ -4,5 +4,5 @@ cmdkey /delete git:https://github.com
 @REM /passに設定するのは、settings -> Developer settings -> Personal access tokens で作成した access token
 cmdkey /generic git:https://github.com /user:user /pass:accesstoken
 @REM github上のアカウントに紐づく情報を登録すると、そのユーザでの操作になる
-git config user.name --global "NAME"
-git config user.email --global "E-MAIL"
+git config --global user.name "NAME"
+git config --global user.email "E-MAIL"
